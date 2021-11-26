4c4047538a1f6e4c2a42545100c914860017354d
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-26 23:46:59 +0900

    FactoryBot設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {'test'}
+    email {Faker::Internet.free_email}
+    password {'ia0000aa'}
+    password_confirmation {password}
+    last_name {'山田'}
+    first_name{'太郎'}
+    last_name_kana{'ヤマダ'}
+    first_name_kana{'タロウ'}
+    birthday{'2000-01-10'}
+    
+  end
+end
