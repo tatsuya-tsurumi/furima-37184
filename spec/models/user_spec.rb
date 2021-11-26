2c2e0e779609c7ebc7084c8a6a01b34e44112aba
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-27 00:11:56 +0900

    テストコード　空のものは保存できない全項目確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,29 +31,30 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it 'last_nameが空では登録できない' do
-        @user.nickname = ''
+        @user.last_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        
+        expect(@user.errors.full_messages).to include("Last name can't be blank", "Last name is invalid")
       end
       it 'first_nameが空では登録できない' do
-        @user.nickname = ''
+        @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("First name can't be blank", "First name is invalid")
       end
       it 'last_name_kanaが空では登録できない' do
-        @user.nickname = ''
+        @user.last_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank", "Last name kana is invalid")
       end
       it 'first_name_kanaが空では登録できない' do
-        @user.nickname = ''
+        @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank", "First name kana is invalid")
       end
       it 'birthdayが空では登録できない' do
-        @user.nickname = ''
+        @user.birthday = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
 
 
