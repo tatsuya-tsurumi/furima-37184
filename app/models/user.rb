6c376cd3b52e6b72e1831594f3b2c84dcc6e763a
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-26 15:17:27 +0900

    新規登録・ログイン用のビューファイル置き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates: nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates: last_name
+    validates: first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates: last_name_kana
+    validates: first_name_kana
+  end
+  validates; birthday, presence: true
+  
+
+
+
+
 end
