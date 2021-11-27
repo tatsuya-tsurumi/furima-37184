04e3ea5af9242da8bc34ec9b7eee76000896c10b
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-28 00:01:26 +0900

    コントローラーにnew，createアクション設定、ストロングパラメーター記述。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
   end
 
   private
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:products, :products_explanation, :category_id, :state_id, :delivery_charge_id, :region_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
-
-  
   
 end
