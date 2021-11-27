28c5e3327ed113ae7187ea40ee90fef23147bf5c
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-27 22:14:15 +0900

    itemsコントローラーへcreateアクション設定。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,14 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def create
+  end
+
   private
   def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
+
+  
   
 end
