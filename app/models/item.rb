2359fe43b49706eebd441d19322e04b559339d36
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-27 22:08:05 +0900

    itemモデルへバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :products, presence: true
+  validates :products_explanation, presence: true
+  validates :category_id, presence: true
+  validates :state_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :region_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  
 end
