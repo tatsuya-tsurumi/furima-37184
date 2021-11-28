bb457e963bfaa0a1797274033594ceaac356f0f7
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-28 10:00:02 +0900

    ActiveHash記述。価格のバリデーション（半角数値，価格に下限・上限）追記。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,24 @@
 class Item < ApplicationRecord
   belongs_to :user
+  
   has_one_attached :image
 
   validates :products, presence: true
   validates :products_explanation, presence: true
-  validates :category_id, presence: true
-  validates :state_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :region_id, presence: true
-  validates :shipping_date_id, presence: true
-  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :state_id, numericality: { other_than: 1 , message:"can't be blank"}
+  validates :delivery_charge_id, numericality: {other_than: 1 , message:"can't be blank"}
+  validates :region_id, numericality: {other_than: 1 , message:"can't be blank"}
+  validates :shipping_date_id, numericality] {other_than: 1 ,message:"can't be blank"}
+  validates :price, presence: true, format: {with: VALID_PRICEL_HALF}, numericality: { only_integer: true,
+    greater_than: 300, less_than: 10000000
   validates :image, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_charge
+  belongs_to :region
+  belongs_to :shipping_date
   
 end
