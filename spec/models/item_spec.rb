7d125be3c4c206b78bd3d7f8be93371360454581
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-29 16:07:18 +0900

    '1'→1へ変更

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,29 +30,29 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Products explanation can't be blank")
       end
       it 'カテゴリーが---では保存できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
 
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態が---では保存できない' do
-        @item.state_id = '1'
+        @item.state_id = 1
         @item.valid?
 
         expect(@item.errors.full_messages).to include("State can't be blank")
       end
       it '配送料の負担が---では保存できない' do
-        @item.delivery_charge_id = '1'
+        @item.delivery_charge_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
       it '発送元の地域が---では保存できない' do
-        @item.region_id = '1'
+        @item.region_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Region can't be blank")
       end
       it '発送までの日数が---では保存できない' do
-        @item.shipping_date_id = '1'
+        @item.shipping_date_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping date can't be blank")
       end
