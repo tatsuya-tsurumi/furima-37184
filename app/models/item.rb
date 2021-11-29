b2a186e537851d204f371a36c9a1cfa29150f8a6
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-29 12:28:57 +0900

    記述ミス修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
   VALID_PRICEL_HALF = /\A[0-9]+\z/
   validates :price, presence: true, format: { with: VALID_PRICEL_HALF }, numericality: { only_integer: true,
-                                                                                         greater_than: 300, less_than: 10_000_000 }
+                                                                                         greater_than: 299, less_than: 10_000_000 }
   validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
