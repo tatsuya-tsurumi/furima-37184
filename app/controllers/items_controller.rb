788001903f2da6eaf6aeeee0c7486f5aba40d0af
tatsuya-tsurumi <turumi4551@gmail.com>
2021-11-30 11:03:46 +0900

    指摘事項修正（destroy削除）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update]
   def index
     @item = Item.all.order('created_at DESC')
   end
