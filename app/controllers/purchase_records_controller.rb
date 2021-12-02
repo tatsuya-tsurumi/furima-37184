577b58c57002a35f049e4c02f38e1e53189d3a72
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-02 19:12:56 +0900

    フォームオブジェクト　index、createアクション記述

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -2,6 +2,8 @@ class PurchaseRecordsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @item = Item.find(params[:item_id])
+    @items = PurchaseRecordShippingAddress.new
   end
 
   def new
@@ -9,6 +11,24 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def create
+   
+    @item = Item.find(params[:item_id])
+    
+    @items = PurchaseRecordShippingAddress.new(purchase_record_params)
+    
+    if @items.valid?
+      @items.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private 
+
+  def purchase_record_params
+    
+    params.require(:purchase_record_shipping_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 
