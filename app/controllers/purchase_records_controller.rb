cc5ae8773424d79828d5d341a0cebee45058dc77
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-03 14:49:31 +0900

    token情報をサーバーへ送る実装、token情報受け取り。

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,5 +1,6 @@
 class PurchaseRecordsController < ApplicationController
   before_action :authenticate_user!
+  
 
   def index
     @item = Item.find(params[:item_id])
@@ -14,9 +15,10 @@ class PurchaseRecordsController < ApplicationController
    
     @item = Item.find(params[:item_id])
     
-    @items = PurchaseRecordShippingAddress.new(purchase_record_params)
+    @items = PurchaseRecordShippingAddress.new(item_params)
     
     if @items.valid?
+      pay_item
       @items.save
       redirect_to root_path
     else
@@ -26,10 +28,21 @@ class PurchaseRecordsController < ApplicationController
 
   private 
 
-  def purchase_record_params
+  def item_params
     
-    params.require(:purchase_record_shipping_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_record_shipping_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number, :price).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: item_params[:token],
+        currency: 'jpy'
+      )
+  end
+
+  
 
 
 end
