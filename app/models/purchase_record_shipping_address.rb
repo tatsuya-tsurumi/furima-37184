0b31e0f109d4cca8ba232b5eb6ec04c874231a9a
tatsuya-tsurumi <turumi4551@gmail.com>
2021-12-03 14:47:30 +0900

    token情報受け取り、電話番号バリデーション追記

--- app/models/purchase_record_shipping_address.rb
+++ app/models/purchase_record_shipping_address.rb
@@ -1,6 +1,6 @@
 class PurchaseRecordShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id
@@ -8,7 +8,8 @@ class PurchaseRecordShippingAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :block
-    validates :phone_number
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
